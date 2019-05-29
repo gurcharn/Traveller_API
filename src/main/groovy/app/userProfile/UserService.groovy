e2fd1940818e33b0f4938440242ae37b74ea1051
Gurcharn Singh Sikka <gurcharan.singh6662@gmail.com>
2019-05-29 21:36:34 +0100

    updated user service

--- src/main/groovy/app/userProfile/UserService.groovy
+++ src/main/groovy/app/userProfile/UserService.groovy
@@ -2,8 +2,6 @@ package app.userProfile
 
 import org.springframework.data.mongodb.repository.MongoRepository
 
-import javax.jws.soap.SOAPBinding
-
 interface UserService extends MongoRepository<User, String> {
 
     User findByUserId(String userId)
