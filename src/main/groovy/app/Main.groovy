80bce433807bd3c1083fe1b08a79063f9d06964e
Gurcharn Singh Sikka <gurcharan.singh6662@gmail.com>
2019-05-29 21:34:31 +0100

    update gradle build file

--- src/main/groovy/app/Main.groovy
+++ src/main/groovy/app/Main.groovy
@@ -1,9 +1,11 @@
 package app
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+@EnableAutoConfiguration
 class Main {
     static void main(String[] args){
         SpringApplication.run( Main, args )
